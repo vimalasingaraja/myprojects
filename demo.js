a7ebd71e502dcb27a1c87bed2bc26ebf04b99658
vimalasingaraja <144030455+vimalasingaraja@users.noreply.github.com>
2023-09-06 16:43:58 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,3 +3,37 @@ const copyText= () => {
 navigator.clipboard.writeText("copied");
   toast.success("Copied..!");
 }
+
+// useContext()
+// ****************************** context file *******************************
+import React, { createContext, useContext, useState } from "react";
+
+const CustomizeContext = createContext();
+
+export const useCustomizeContext = () => useContext(CustomizeContext);
+
+export const CustomizeProvider = ({ children }) => {
+  const [Metamaskaddress, setMetamaskaddress] = useState();
+  
+  const Commonlist = {
+    Metamaskaddress, setMetamaskaddress,
+  };
+
+  return (
+    <CustomizeContext.Provider
+      value={{
+        Commonlist,
+      }}
+    >
+      {children}
+    </CustomizeContext.Provider>
+  );
+}
+
+// **************************************component **********************************
+import { useCustomizeContext } from "../../Context/context";
+
+
+ const {
+    Commonlist: { Metamaskaddress },
+  } = useCustomizeContext();
