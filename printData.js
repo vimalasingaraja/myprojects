f2bbc6e9d087d54f9d25dc20d477627714971841
vimalasingaraja <144030455+vimalasingaraja@users.noreply.github.com>
2024-01-03 13:30:04 +0530

    Create printData.js
    table data into pdf format

---
+++ printData.js
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+
+const PrintTable = () => {
+
+    const contentPDF = useRef();
+
+    const data = [
+        { name: "Anom", age: 19, gender: "Male" },
+        { name: "Megha", age: 19, gender: "Female" },
+        { name: "Subham", age: 25, gender: "Male" },
+    ]
+
+    const generatePdf=useReactToPrint({
+        content: ()=>contentPDF.current,
+        documentTitle: "Customerdata",
+        onAfterPrint:()=>alert("Data saved in PDF")
+    })
+
+    return (
+        <div>
+            <div ref={contentPDF}>
+
+                <h2>Customer detail
+                <button onClick={generatePdf}>Print</button>
+                </h2>
+
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Gender</th>
+                    </tr>
+
+                    {data.map((item) => (
+                        <tr>
+                            <td>{item.name}</td>
+                            <td>{item.age}</td>
+                            <td>{item.gender}</td>
+                        </tr>
+                    ))}
+
+                   
+                </table >
+            </div>
+        </div >
+    )
+}
+
+export default PrintTable;
